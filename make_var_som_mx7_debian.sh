f43085ed7a49623459612d62ea730434dc6410a5
Eran Matityahu <eran.m@variscite.com>
2017-07-05 14:21:39 +0300

    linux: Update to latest branch

--- make_var_som_mx7_debian.sh
+++ make_var_som_mx7_debian.sh
@@ -43,7 +43,7 @@ readonly G_VARISCITE_PATH="${DEF_BUILDENV}/variscite"
 ## LINUX kernel: git, config, paths and etc
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-2.6-imx.git"
-readonly G_LINUX_KERNEL_BRANCH="imx-rel_imx_4.1.15_2.0.0_ga-var01"
+readonly G_LINUX_KERNEL_BRANCH="imx-rel_imx_4.1.15_2.0.0_ga-var02"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx7-var-som_defconfig'
 readonly G_LINUX_DTB='imx7d-var-som-emmc.dtb imx7d-var-som-nand.dtb'
 
