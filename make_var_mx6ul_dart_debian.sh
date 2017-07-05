fbc609a32a872ddecb65419acf93e2133418eaa2
Eran Matityahu <eran.m@variscite.com>
2017-07-05 14:55:05 +0300

    Add U-Boot splash image

--- make_var_mx6ul_dart_debian.sh
+++ make_var_mx6ul_dart_debian.sh
@@ -495,6 +495,7 @@ rm -f user-stage
 	install -m 0755 ${G_VARISCITE_PATH}/issue.net ${ROOTFS_BASE}/etc/
 	install -m 0755 ${G_VARISCITE_PATH}/hostapd.conf ${ROOTFS_BASE}/etc/
 	install -m 0755 ${G_VARISCITE_PATH}/rc.local ${ROOTFS_BASE}/etc/
+	install -m 0644 ${G_VARISCITE_PATH}/splash.bmp ${ROOTFS_BASE}/boot/
 
 ## added alsa default configs ##
 	install -m 0666 ${G_VARISCITE_PATH}/asound.state ${ROOTFS_BASE}/var/lib/alsa/
