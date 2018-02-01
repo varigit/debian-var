0d63b66eab61d03849495cd4bb779947854d7d0d
Eran Matityahu <eran.m@variscite.com>
2018-02-01 19:48:18 +0200

    linux: Update to latest revision
    Include the following commit:
    
    aa811714bad dart-6ul: Reducing the speed of SDHC1 clock to 132Mhz
    
    Signed-off-by: Eran Matityahu <eran.m@variscite.com>

--- make_var_mx6ul_dart_debian.sh
+++ make_var_mx6ul_dart_debian.sh
@@ -42,7 +42,7 @@ readonly G_VARISCITE_PATH="${DEF_BUILDENV}/variscite"
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="imx_4.9.11_1.0.0_ga-var01"
-readonly G_LINUX_KERNEL_REV="8acc1a483056f0174473fc1555413686c245f975"
+readonly G_LINUX_KERNEL_REV="aa811714badb0d69660a6efe09ef97324502bd92"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_defconfig'
 readonly G_LINUX_DTB='imx6ul-var-dart-emmc_wifi.dtb imx6ul-var-dart-nand_wifi.dtb imx6ul-var-dart-sd_emmc.dtb imx6ul-var-dart-sd_nand.dtb imx6ull-var-dart-emmc_wifi.dtb imx6ull-var-dart-sd_emmc.dtb imx6ull-var-dart-nand_wifi.dtb imx6ull-var-dart-sd_nand.dtb imx6ul-var-dart-5g-emmc_wifi.dtb imx6ull-var-dart-5g-emmc_wifi.dtb imx6ul-var-dart-5g-nand_wifi.dtb imx6ull-var-dart-5g-nand_wifi.dtb'
 
