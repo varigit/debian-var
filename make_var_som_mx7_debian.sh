4058da8d970de2dcdf18d704aa93000314d2804f
Eran Matityahu <eran.m@variscite.com>
2018-03-14 16:24:39 +0200

    linux: Update to latest revision
    Include the following commits:
    
    c178e1cef4a imx7d-var-som: Limit the max cpu freq. to 1 GHz
    b83421f424f var-som-mx7: Adjust the nand mtd partitions for spl
    7f27fb72363 Fix OV5640 driver for S_FMT and MX6S SOC drivers
    aa811714bad dart-6ul: Reducing the speed of SDHC1 clock to 132Mhz
    
    Signed-off-by: Eran Matityahu <eran.m@variscite.com>

--- make_var_som_mx7_debian.sh
+++ make_var_som_mx7_debian.sh
@@ -42,7 +42,7 @@ readonly G_VARISCITE_PATH="${DEF_BUILDENV}/variscite"
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="imx_4.9.11_1.0.0_ga-var01"
-readonly G_LINUX_KERNEL_REV="8acc1a483056f0174473fc1555413686c245f975"
+readonly G_LINUX_KERNEL_REV="c178e1cef4ae85dd8a1b648546b71db5e2893e2d"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_defconfig'
 readonly G_LINUX_DTB='imx7d-var-som-emmc.dtb imx7d-var-som-emmc-m4.dtb imx7d-var-som-nand.dtb imx7d-var-som-nand-m4.dtb'
 
