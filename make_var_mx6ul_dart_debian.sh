b74bfd53c6342a984c0d6b8e19e6d022f3ed7e84
Pierluigi Passaro <pierluigi.p@variscite.com>
2018-04-05 17:04:44 +0200

    linux: update to latest revision
    Include the following commits:
    a1ac1724c29 var-som-mx7: reworked dtsi audio routing
    36c669c5599 imx6ul-var-dart: reworked dtsi audio configuration

--- make_var_mx6ul_dart_debian.sh
+++ make_var_mx6ul_dart_debian.sh
@@ -42,7 +42,7 @@ readonly G_VARISCITE_PATH="${DEF_BUILDENV}/variscite"
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="imx_4.9.11_1.0.0_ga-var01"
-readonly G_LINUX_KERNEL_REV="46a5cab4df6b501eebcc820c55689493119ea69f"
+readonly G_LINUX_KERNEL_REV="a1ac1724c29c3d2051b68d8c0d743de22e0632e3"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_defconfig'
 readonly G_LINUX_DTB='imx6ul-var-dart-emmc_wifi.dtb imx6ul-var-dart-nand_wifi.dtb imx6ul-var-dart-sd_emmc.dtb imx6ul-var-dart-sd_nand.dtb imx6ull-var-dart-emmc_wifi.dtb imx6ull-var-dart-sd_emmc.dtb imx6ull-var-dart-nand_wifi.dtb imx6ull-var-dart-sd_nand.dtb imx6ul-var-dart-5g-emmc_wifi.dtb imx6ull-var-dart-5g-emmc_wifi.dtb imx6ul-var-dart-5g-nand_wifi.dtb imx6ull-var-dart-5g-nand_wifi.dtb'
 
