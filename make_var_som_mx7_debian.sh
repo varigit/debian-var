ece67eab01c62338d5f7559923fc9437b3b9cbec
Pierluigi Passaro <pierluigi.p@variscite.com>
2018-09-14 10:06:11 +0200

    update uboot/kernel compiler to gcc 6.3.1
    This compiler update is required to allow building a bootable SPL binary upon i.MX7 "revision D" patch integration: with previous gcc 4.9, SPL hangs.

--- make_var_som_mx7_debian.sh
+++ make_var_som_mx7_debian.sh
@@ -66,12 +66,12 @@ readonly G_BCM_FW_GIT_BRANCH="imx-rel_imx_4.1.15_2.0.1_ga-var01"
 readonly G_UBI_FILE_NAME='rootfs.ubi.img'
 
 ## CROSS_COMPILER config and paths
-readonly G_CROSS_COMPILEER_PATH="${G_TOOLS_PATH}/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf/bin"
+readonly G_EXT_CROSS_COMPILER_NAME='gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf'
+readonly G_CROSS_COMPILEER_PATH="${G_TOOLS_PATH}/${G_EXT_CROSS_COMPILER_NAME}/bin"
 #readonly G_CROSS_COMPILEER_PREFFIX="arm-none-eabi-"
 readonly G_CROSS_COMPILEER_PREFFIX="arm-linux-gnueabihf-"
 readonly G_CROSS_COMPILEER_JOPTION="-j 4"
-readonly G_EXT_CROSS_COMPILER_NAME='gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf.tar.xz'
-readonly G_EXT_CROSS_COMPILER_LINK="http://releases.linaro.org/components/toolchain/binaries/4.9-2016.02/arm-linux-gnueabihf/${G_EXT_CROSS_COMPILER_NAME}"
+readonly G_EXT_CROSS_COMPILER_LINK="http://releases.linaro.org/components/toolchain/binaries/6.3-2017.05/arm-linux-gnueabihf/${G_EXT_CROSS_COMPILER_NAME}.tar.xz"
 
 ############## user rootfs packages ##########
 readonly G_USER_PACKAGES=""
