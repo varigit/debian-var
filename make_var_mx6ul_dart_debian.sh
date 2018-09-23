070bb89a32c6768d0987c5e2838cb6c5fc4111a8
Pierluigi Passaro <pierluigi.p@variscite.com>
2018-09-23 10:11:00 +0200

    make_var_mx6ul_dart_debian: kernel update
    switch to kernel branch imx_4.9.88_2.0.0_ga-var01

--- make_var_mx6ul_dart_debian.sh
+++ make_var_mx6ul_dart_debian.sh
@@ -41,8 +41,8 @@ readonly G_VARISCITE_PATH="${DEF_BUILDENV}/variscite"
 ## LINUX kernel: git, config, paths and etc
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
-readonly G_LINUX_KERNEL_BRANCH="imx_4.9.11_1.0.0_ga-var01"
-readonly G_LINUX_KERNEL_REV="86c54d808ad9f1b39f8641d2da2e6da4bfff9f30"
+readonly G_LINUX_KERNEL_BRANCH="imx_4.9.88_2.0.0_ga-var01"
+readonly G_LINUX_KERNEL_REV="1b28049c72c9f9300b6eb0ca96a4fb47233831c5"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_defconfig'
 readonly G_LINUX_DTB='imx6ul-var-dart-emmc_wifi.dtb imx6ul-var-dart-nand_wifi.dtb imx6ul-var-dart-sd_emmc.dtb imx6ul-var-dart-sd_nand.dtb imx6ull-var-dart-emmc_wifi.dtb imx6ull-var-dart-sd_emmc.dtb imx6ull-var-dart-nand_wifi.dtb imx6ull-var-dart-sd_nand.dtb imx6ul-var-dart-5g-emmc_wifi.dtb imx6ull-var-dart-5g-emmc_wifi.dtb imx6ul-var-dart-5g-nand_wifi.dtb imx6ull-var-dart-5g-nand_wifi.dtb'
 
