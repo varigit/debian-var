c9d9f43b4e4e5aa897d2218511c71b416613e41d
Eran Matityahu <eran.m@variscite.com>
2019-03-07 18:49:20 +0200

    linux: Update to latest revision
    Include the following commits:
    
    ec7d7d8ccb4 ov5640: Auto Focus support
    68babcf9720 camera: add focus control to mxc core
    aaecf096161 ARM: dts: imx6ull: specify proper clocks for the PWM nodes
    
    Signed-off-by: Eran Matityahu <eran.m@variscite.com>

--- make_var_som_mx6_debian.sh
+++ make_var_som_mx6_debian.sh
@@ -42,7 +42,7 @@ readonly G_VARISCITE_PATH="${DEF_BUILDENV}/variscite"
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="imx_4.9.88_2.0.0_ga-var01"
-readonly G_LINUX_KERNEL_REV="a2cc75c7c42772a1147557ff6018af11fc37ae44"
+readonly G_LINUX_KERNEL_REV="ec7d7d8ccb4960ee382c97f3b08db16086382d4b"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_defconfig'
 readonly G_LINUX_DTB='imx6dl-var-som-cap.dtb imx6dl-var-som-res.dtb imx6dl-var-som-solo-cap.dtb imx6dl-var-som-solo-res.dtb imx6dl-var-som-solo-vsc.dtb imx6dl-var-som-vsc.dtb imx6q-var-dart.dtb imx6q-var-som-cap.dtb imx6q-var-som-res.dtb imx6q-var-som-vsc.dtb imx6qp-var-som-cap.dtb imx6qp-var-som-res.dtb imx6qp-var-som-vsc.dtb'
 
