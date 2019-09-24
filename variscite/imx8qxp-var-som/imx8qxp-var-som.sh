8693f99800a64bee5410a19999d7dc11f61e933d
Eran Matityahu <eran.m@variscite.com>
2019-09-24 15:04:09 +0300

    imx8qxp-var-som: linux: Update to latest revision
    Include the following commits:
    
    10655422aa2 arm64: imx8_var_defconfig: Enable CONFIGs needed for Docker
    67889cc6021 dm: fix incomplete request_queue initialization
    7c6dfab8d70 imx8qm_var_som: Disable resistive touch controller
    a8bbbb66531 imx8qm_var_som: Add HDMI and DP support
    98d130bc1dd imx8qm_var_som: switch to fsl,imx-audio-wm8904 sound glue code
    fb7d38beae3 ASoC: fsl-asoc-card: add WM8904 support
    dea32f548a6 fsl-asoc-card: Be safer when detecting codec
    e04030ab8ea Revert "ASoC: fsl-asoc-card: add WM8904 support"
    01d42f27528 imx8qxp_var_som: Add support for OV5640 parallel CSI sensor
    2c90ee32ea9 imx8qxp_var_som: Cosmetic DTS fixes
    
    Signed-off-by: Eran Matityahu <eran.m@variscite.com>

--- variscite/imx8qxp-var-som/imx8qxp-var-som.sh
+++ variscite/imx8qxp-var-som/imx8qxp-var-som.sh
@@ -10,7 +10,7 @@ readonly G_UBOOT_NAME_FOR_EMMC='imx-boot-sd.bin'
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="imx_4.14.98_2.0.0_ga_var01"
-readonly G_LINUX_KERNEL_REV="81f9fa0fc4e4faf7b782d3c3c385d29f61fca121"
+readonly G_LINUX_KERNEL_REV="10655422aa2d846ea24bb904712e2fd76b3723ba"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx8_var_defconfig'
 G_LINUX_DTB="freescale/fsl-imx8qxp-var-som-sd.dtb
              freescale/fsl-imx8qxp-var-som-wifi.dtb"
