25e31056262f005ee85e78ad24ca02f1ddc5c9d1
Eran Matityahu <eran.m@variscite.com>
2019-09-24 14:57:35 +0300

    imx8qxp-var-som: u-boot: Update to latest revision
    Include the following commits:
    
    39aa5d3b6da imx8qm_var_som: fix console in android bootargs
    a275ece5b5e imx8qm_var_som: Pass optional kernel arguments in MMC boot
    6bc2e0eacca imx8qm_var_som: Disable CONFIG_VIDEO
    d58fabcc9b2 i.MX8QM: Remove dependency of HDP firmware loading on CONFIG_VIDEO
    edcd0a99f70 imx8qxp_var_som, imx8qm_var_som: Add flexibility to mmc part selection
    97bb8de853f imx8qm_var_som: add android support
    928bf031d25 imx8qxp_var_som: add android support
    
    Signed-off-by: Eran Matityahu <eran.m@variscite.com>

--- variscite/imx8qxp-var-som/imx8qxp-var-som.sh
+++ variscite/imx8qxp-var-som/imx8qxp-var-som.sh
@@ -2,7 +2,7 @@
 readonly G_UBOOT_SRC_DIR="${DEF_SRC_DIR}/uboot"
 readonly G_UBOOT_GIT="https://github.com/varigit/uboot-imx.git"
 readonly G_UBOOT_BRANCH="imx_v2018.03_4.14.98_2.0.0_ga_var01"
-readonly G_UBOOT_REV="82b111752596b4ed3c8292140516078a26cb0450"
+readonly G_UBOOT_REV="39aa5d3b6da74a8f69335a6d178bfa6a3f0fd59f"
 G_UBOOT_DEF_CONFIG_MMC='imx8qxp_var_som_defconfig'
 readonly G_UBOOT_NAME_FOR_EMMC='imx-boot-sd.bin'
 
