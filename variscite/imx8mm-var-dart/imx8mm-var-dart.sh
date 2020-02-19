81ef22698827785fbb5389e8b2f91a29c1e4e5ab
Eran Matityahu <eran.m@variscite.com>
2020-02-19 17:02:29 +0200

    imx8mm-var-dart: linux: Update to latest revision
    The relevant included commits are:
    
    f027cde8d8f imx8mm-var-dart: remove rpmgs node when Cortex-M4 is not used
    e692c306236 imx8mm-var-dart: add Cortex-M4 related device tree
    eb6112fd897 imx8mm-var-dart: add Cortex-M4 related device tree
    ffbb4f5dd61 MLK-21102 driver: thermal: fix the tmu out of range issue on imx8mm
    
    Signed-off-by: Eran Matityahu <eran.m@variscite.com>

--- variscite/imx8mm-var-dart/imx8mm-var-dart.sh
+++ variscite/imx8mm-var-dart/imx8mm-var-dart.sh
@@ -12,7 +12,7 @@ readonly G_UBOOT_NAME_FOR_EMMC='imx-boot-sd.bin'
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="imx_4.14.78_1.0.0_ga_var01"
-readonly G_LINUX_KERNEL_REV="09a8b5955516af5a54219b521eda73f286dd0e15"
+readonly G_LINUX_KERNEL_REV="f027cde8d8f184c958500950efc05ac9ac6544ce"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx8_var_defconfig'
 G_LINUX_DTB="freescale/fsl-imx8mm-var-dart.dtb"
 
