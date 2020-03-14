909588c138b0e450c5c92a2b2bd4d833b7f126da
Harshesh Valera <harshesh.v@variscite.com>
2020-03-13 17:25:27 -0700

    imx8qxp-var-som: u-boot: Update to latest revision
    Include the following commits:
    42342c4846a imx8qxp_var_som: Set default DRAM size to 2GB
    2a835695256 splash: Fix more compilation warnings on 64-bit platforms
    1a90b78debe imx8qxp_var_som: spl: Remove spl_dram_init
    5903591a472 imx8qxp_var_som: spl: Explicitly configure SD/WIFI SDIO routing
    6d92aced2fa imx8qxp-var-som: Add support for reading DRAM size from the EEPROM
    3b9bef39e37 Variscite i.MX8 boards: Add common DRAM and memory map routines
    d5d1e771c83 imx8: dram: Allow overriding DRAM init routines in board code.
    01be2e188e8 imx8qxp-var-som: Add SPL support
    3d0c1d244fe autoboot: SDP: Stop setting the environment to default when booting from USB
    72c8fa4c9fd usb: ehci-mx6: Fix USB type detection
    63fddd882b2 imx8qm_var_som, imx8qxp_var_som: Reorganize common code.
    
    Signed-off-by: Harshesh Valera <harshesh.v@variscite.com>

--- variscite/imx8qxp-var-som/imx8qxp-var-som.sh
+++ variscite/imx8qxp-var-som/imx8qxp-var-som.sh
@@ -4,7 +4,7 @@ readonly ARCH_CPU="64BIT"
 readonly G_UBOOT_SRC_DIR="${DEF_SRC_DIR}/uboot"
 readonly G_UBOOT_GIT="https://github.com/varigit/uboot-imx.git"
 readonly G_UBOOT_BRANCH="imx_v2018.03_4.14.98_2.0.0_ga_var01"
-readonly G_UBOOT_REV="ea9c11cc181b1a59fcb8e3dbb1d55a82dd1fa0b3"
+readonly G_UBOOT_REV="42342c4846a45a950e34bfae198e5e2b19ef6c6e"
 G_UBOOT_DEF_CONFIG_MMC='imx8qxp_var_som_defconfig'
 readonly G_UBOOT_NAME_FOR_EMMC='imx-boot-sd.bin'
 
