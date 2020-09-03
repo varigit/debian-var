79294dca7e63122e6045df0d97005eb378f02b9a
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2020-09-03 14:47:35 -0700

    var-som-mx7: Fix https://github.com/varigit/debian-var/issues/9
    make_var_som_mx7_debian.sh: Add loop device support
    
    Signed-off-by: Harshesh Valera <harshesh.v@variscite.com>

--- make_var_som_mx7_debian.sh
+++ make_var_som_mx7_debian.sh
@@ -799,7 +799,8 @@ function make_sdcard() {
 	};
 
 	local part=""
-	if [ `echo ${LPARAM_BLOCK_DEVICE} | grep -c mmcblk` -ne 0 ]; then
+	if [ `echo ${LPARAM_BLOCK_DEVICE} | grep -c mmcblk` -ne 0 ]; \
+		|| [[ ${LPARAM_BLOCK_DEVICE} == *"loop"* ]] ; then
 		part="p"
 	fi
 
