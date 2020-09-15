6bb93aaee4e089c5116839593b02abdba23282b9
Eran Matityahu <eran.m@variscite.com>
2020-09-15 20:07:48 +0300

    imx8mm-var-dart: linux: Update to latest revision
    The relevant included commit is:
    
    8aeff32a1a1 imx8mm_var_dart: Correct ldo1/ldo2 voltage for DART-MX8MM-MINI and VAR-SOM-MX8MM-MINI
    
    Signed-off-by: Eran Matityahu <eran.m@variscite.com>

--- variscite/imx8mm-var-dart/imx8mm-var-dart.sh
+++ variscite/imx8mm-var-dart/imx8mm-var-dart.sh
@@ -12,7 +12,7 @@ readonly G_UBOOT_NAME_FOR_EMMC='imx-boot-sd.bin'
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="imx_4.19.35_1.1.0_var01"
-readonly G_LINUX_KERNEL_REV="4faf4bcc4720d64e0b2f4b5db4435b8f4ef30990"
+readonly G_LINUX_KERNEL_REV="dc7a2fde4ab056303e2011347438bdae23036c5e"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx8_var_defconfig'
 G_LINUX_DTB="freescale/fsl-imx8mm-var-dart.dtb
 	freescale/fsl-imx8mm-var-dart-m4.dtb
