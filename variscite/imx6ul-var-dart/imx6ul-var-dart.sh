be4f62d0f7a79f1a8544b9e1a96b8f6d06ddff6f
FrancescoFerraro <francesco.f@variscite.com>
2021-07-05 18:02:58 +0200

    imx6ul-var-dart: uboot-imx: linux-imx: Update to latest
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- variscite/imx6ul-var-dart/imx6ul-var-dart.sh
+++ variscite/imx6ul-var-dart/imx6ul-var-dart.sh
@@ -3,8 +3,8 @@ readonly ARCH_CPU="32BIT"
 # U-Boot
 readonly G_UBOOT_SRC_DIR="${DEF_SRC_DIR}/uboot"
 readonly G_UBOOT_GIT="https://github.com/varigit/uboot-imx.git"
-readonly G_UBOOT_BRANCH="imx_v2018.03_4.14.78_1.0.0_ga_var01"
-readonly G_UBOOT_REV="b399abdade7c86ec6e4467e1d7a903df3cad6ec2"
+readonly G_UBOOT_BRANCH="imx_v2018.03_4.14.78_1.0.0_ga_var02"
+readonly G_UBOOT_REV="32670c4e21bf8d1de08e1d1b1f9bfe42c23bc6af"
 readonly G_UBOOT_DEF_CONFIG_MMC='mx6ul_var_dart_mmc_defconfig'
 readonly G_UBOOT_DEF_CONFIG_NAND='mx6ul_var_dart_nand_defconfig'
 readonly G_UBOOT_NAME_FOR_EMMC='u-boot.img.mmc'
@@ -15,8 +15,8 @@ readonly G_SPL_NAME_FOR_NAND='SPL.nand'
 # Linux kernel
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
-readonly G_LINUX_KERNEL_BRANCH="imx_4.14.78_1.0.0_ga_var01"
-readonly G_LINUX_KERNEL_REV="8e54a4b719e6ec0a9e051b0f266036565a555e7d"
+readonly G_LINUX_KERNEL_BRANCH="5.4-2.1.x-imx_var01"
+readonly G_LINUX_KERNEL_REV="601330bfa4d56345159cb571f90b0e19adebb1fd"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_defconfig'
 G_LINUX_DTB="imx6ull-var-dart-6ulcustomboard-emmc-sd-card.dtb
 	imx6ull-var-dart-6ulcustomboard-emmc-wifi.dtb
