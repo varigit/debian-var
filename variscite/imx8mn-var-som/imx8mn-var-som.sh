628e81a9cd2ce2da928e137aba925e02e3283f8c
Harshesh Valera <harshesh.v@variscite.com>
2021-07-19 21:41:28 -0700

    imx8mn-var-som: linux: Update to latest revision
    The relevant included commits are:
    
    0243069daad Revert "gpio: pca953x: Disable PM routines"
    c1a013ba2c2 ARM: configs: Variscite: Enable USB_HSIC_USB3053
    343970224e3 ARM: configs: Variscite: disable EVBUG module
    a5b5ecfa3d4 Revert "pad-imx8qm: add defines for wakeup trigger types"
    4d756b88212 ARM64: dts: imx8: Variscite: add WM8904 DRC support for all SoMs
    bd37875a0f5 ASoC: wm8904: add DMIC support
    eed4bc4e9c3 ASoC: wm8904: extend device tree support
    b53c0e35810 Revert "ASoC: wm8904: add DMIC support and allow enabling it"
    
    Signed-off-by: Harshesh Valera <harshesh.v@variscite.com>

--- variscite/imx8mn-var-som/imx8mn-var-som.sh
+++ variscite/imx8mn-var-som/imx8mn-var-som.sh
@@ -12,7 +12,7 @@ readonly G_UBOOT_NAME_FOR_EMMC='imx-boot-sd.bin'
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="5.4-2.1.x-imx_var01"
-readonly G_LINUX_KERNEL_REV="786d1c620c9279028a5c13770721c0be1db7ee95"
+readonly G_LINUX_KERNEL_REV="601330bfa4d56345159cb571f90b0e19adebb1fd"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx8_var_defconfig'
 G_LINUX_DTB="freescale/imx8mn-var-som-symphony.dtb
 	freescale/imx8mn-var-som-symphony-root.dtb
