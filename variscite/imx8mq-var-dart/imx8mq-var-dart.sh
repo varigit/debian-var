1459916e169063cd4da6c9a965498d9c6db3e080
Felix Radensky <felix.r@variscite.com>
2021-08-23 16:14:40 +0300

    imx8mq-var-dart: linux: Update to latest revision
    Include the following commits:
    
    f0e3c7acf46 imx8mq-var-dart: Add PCIE PHY power regulator
    2226e2d662a imx8mq-var-dart: Remove "hard-wired" PCIE0 controller property
    411045d1922 MLK-25349-3 PCI: imx: clear vreg bypass when pcie vph voltage is 3v3
    bb5aa0f0890 MLK-25349-2 arm64: dts: imx8mq-evk: add one regulator used to power up pcie phy
    b84f833c561 MLK-25349-1 dt-bindings: imx6q-pcie: add one regulator used to power up pcie phy

--- variscite/imx8mq-var-dart/imx8mq-var-dart.sh
+++ variscite/imx8mq-var-dart/imx8mq-var-dart.sh
@@ -13,7 +13,7 @@ readonly G_UBOOT_NAME_FOR_EMMC_DP='imx-boot-sd-dp.bin'
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="5.4-2.1.x-imx_var01"
-readonly G_LINUX_KERNEL_REV="601330bfa4d56345159cb571f90b0e19adebb1fd"
+readonly G_LINUX_KERNEL_REV="f0e3c7acf46097fdd1fd9fd3279c26faf8226b4e"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx8mq_var_dart_defconfig'
 G_LINUX_DTB="freescale/imx8mq-var-dart-dt8mcustomboard-legacy-m4-sd-dp.dtb
 	freescale/imx8mq-var-dart-dt8mcustomboard-legacy-m4-sd-hdmi.dtb
