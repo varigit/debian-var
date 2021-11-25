b362709e57efc898701346a3599e57e8eed89a94
Alexander Herfurtner <alexander.h@variscite.com>
2021-11-25 09:03:12 +0100

    var-som-mx7: Align to Debian Bullseye
    Updated Debian mirror, kernel, U-Boot, sdma and bcm.
    
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>

--- variscite/var-som-mx7/var-som-mx7.sh
+++ variscite/var-som-mx7/var-som-mx7.sh
@@ -3,8 +3,8 @@ readonly ARCH_CPU="32BIT"
 # U-Boot
 readonly G_UBOOT_SRC_DIR="${DEF_SRC_DIR}/uboot"
 readonly G_UBOOT_GIT="https://github.com/varigit/uboot-imx.git"
-readonly G_UBOOT_BRANCH="imx_v2018.03_4.14.78_1.0.0_ga_var01"
-readonly G_UBOOT_REV="ddc95a6e5b319d6a649a81a5fc5a26ca3c117659"
+readonly G_UBOOT_BRANCH="imx_v2018.03_4.14.78_1.0.0_ga_var02"
+readonly G_UBOOT_REV="32670c4e21bf8d1de08e1d1b1f9bfe42c23bc6af"
 readonly G_UBOOT_DEF_CONFIG_MMC='mx7dvar_som_defconfig'
 readonly G_UBOOT_DEF_CONFIG_NAND='mx7dvar_som_nand_defconfig'
 readonly G_UBOOT_NAME_FOR_EMMC='u-boot.img.mmc'
@@ -15,8 +15,8 @@ readonly G_SPL_NAME_FOR_NAND='SPL.nand'
 # Linux kernel
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
-readonly G_LINUX_KERNEL_BRANCH="imx_4.14.78_1.0.0_ga_var01"
-readonly G_LINUX_KERNEL_REV="8e54a4b719e6ec0a9e051b0f266036565a555e7d"
+readonly G_LINUX_KERNEL_BRANCH="5.4-2.1.x-imx_var01"
+readonly G_LINUX_KERNEL_REV="5227ff0e2a5ca08777d926a440244975ffb0d095"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_defconfig'
 G_LINUX_DTB="imx7d-var-som-emmc.dtb
 	imx7d-var-som-nand.dtb
@@ -31,13 +31,16 @@ KERNEL_DTB_IMAGE_PATH="arch/arm/boot/dts/"
 readonly G_IMX_SDMA_FW_SRC_DIR="${DEF_SRC_DIR}/linux-firmware"
 readonly G_IMX_SDMA_FW_GIT="git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git"
 readonly G_IMX_SDMA_FW_GIT_BRANCH="master"
-readonly G_IMX_SDMA_FW_GIT_REV="710963fe53ee3f227556d36839df3858daf6e232"
+readonly G_IMX_SDMA_FW_GIT_REV="d79c26779d459063b8052b7fe0a48bce4e08d0d9"
 
 # Broadcom BT/WIFI firmware
 readonly G_BCM_FW_SRC_DIR="${DEF_SRC_DIR}/bcmfw"
 readonly G_BCM_FW_GIT="https://github.com/varigit/bcm_4343w_fw.git"
-readonly G_BCM_FW_GIT_BRANCH="6.0.0.121"
-readonly G_BCM_FW_GIT_REV="7bce9b69b51ffd967176c1597feed79305927370"
+readonly G_BCM_FW_GIT_BRANCH="8.2.0.16"
+readonly G_BCM_FW_GIT_REV="8081cd2bddb1569abe91eb50bd687a2066a33342"
 
 # ubi
 readonly G_UBI_FILE_NAME='rootfs.ubi.img'
+
+# default mirror
+readonly DEF_DEBIAN_MIRROR="https://snapshot.debian.org/archive/debian/20210813T203009Z/"
