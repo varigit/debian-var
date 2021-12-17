209c62933b06af937a4b7e6b8bf305e298ec43d9
Pierluigi Passaro <pierluigi.p@variscite.com>
2021-12-18 00:20:16 +0100

    imx8mn-var-som: linux-variscite: update to latest
    Include the following:
    
    8723d4365c8 imx8mn-var-som: update the polarity of camera reset GPIO
    2731a984582 imx8_var_defconfig: add full docker support
    8697f79287c imx8mq_var_dart_defconfig: add full docker support
    63c1015da97 imx8mq-var-dart: Introduce support for pcal6408
    2a53e3b9210 imx8mm-var-dart: Introduce support for pcal6408
    b58250c6dbc gpio: pca953x: Introduce standard-regs-fallback property
    97b5f35cd58 gpio: pca953x: Introduce support for nxp,pcal6408
    f0f2f55eb43 imx8qm-var-som: Introduce support for i.MX8QP SoC
    53774d29747 imx8qm-var-som: enable internal pullups for i2c0, i2c1, i2c3, i2c4
    dbe97925c58 ARM: dts: VAR-SOM-MX6: force enet_clk to 125 MHz
    d731d8ac5d1 Revert 'Revert 'gpio: pca953x: Disable PM routines''
    5227ff0e2a5 ARM: configs: Variscite: update to latest docker requirements
    c6cc913851a MLK-14852 ARM: dts: fix imx7d boot spi dma setup error
    b120913ad92 dts: imx7d-var-som: fix compile warnings
    20b2308008e ARM: dts: imx6ul: add sdma support to uarts
    e189eabf946 Revert "MLK-14801 input: touch: ads7846: fix the pressure_max setting"
    33049b971e6 sn65dsi83: Add device tree property to swap LVDS Channel A/B
    linux-variscite: Update to latest v5.4.142
    f0e3c7acf46 imx8mq-var-dart: Add PCIE PHY power regulator
    2226e2d662a imx8mq-var-dart: Remove "hard-wired" PCIE0 controller property
    411045d1922 MLK-25349-3 PCI: imx: clear vreg bypass when pcie vph voltage is 3v3
    bb5aa0f0890 MLK-25349-2 arm64: dts: imx8mq-evk: add one regulator used to power up pcie phy
    b84f833c561 MLK-25349-1 dt-bindings: imx6q-pcie: add one regulator used to power up pcie phy
    
    Signed-off-by: Pierluigi Passaro <pierluigi.p@variscite.com>

--- variscite/imx8mn-var-som/imx8mn-var-som.sh
+++ variscite/imx8mn-var-som/imx8mn-var-som.sh
@@ -12,7 +12,7 @@ readonly G_UBOOT_NAME_FOR_EMMC='imx-boot-sd.bin'
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="5.4-2.1.x-imx_var01"
-readonly G_LINUX_KERNEL_REV="601330bfa4d56345159cb571f90b0e19adebb1fd"
+readonly G_LINUX_KERNEL_REV="8723d4365c823cb6c82e86d180dc6ed2cdda609d"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx8_var_defconfig'
 G_LINUX_DTB="freescale/imx8mn-var-som-symphony.dtb
 	freescale/imx8mn-var-som-symphony-root.dtb
