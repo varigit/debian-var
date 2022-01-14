31d9c52e023fd6505e7d0fc5b4d92990f7b8c71a
Harshesh Valera <harshesh.v@variscite.com>
2022-01-14 14:47:53 -0800

    imx6ul_var_dart: Fix linux-firmware branch from master to main
    master branch is renamed to main
    
    Signed-off-by: Harshesh Valera <harshesh.v@variscite.com>

--- variscite/imx6ul-var-dart/imx6ul-var-dart.sh
+++ variscite/imx6ul-var-dart/imx6ul-var-dart.sh
@@ -50,7 +50,7 @@ KERNEL_DTB_IMAGE_PATH="arch/arm/boot/dts/"
 # SDMA Firmware
 readonly G_IMX_SDMA_FW_SRC_DIR="${DEF_SRC_DIR}/linux-firmware"
 readonly G_IMX_SDMA_FW_GIT="git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git"
-readonly G_IMX_SDMA_FW_GIT_BRANCH="master"
+readonly G_IMX_SDMA_FW_GIT_BRANCH="main"
 readonly G_IMX_SDMA_FW_GIT_REV="710963fe53ee3f227556d36839df3858daf6e232"
 
 # Broadcom BT/WIFI firmware
