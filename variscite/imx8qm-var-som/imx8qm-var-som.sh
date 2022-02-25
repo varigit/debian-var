23d1daab1107d69025983d9129c0da9cfa2781cb
Alifer Moraes <alifer.m@variscite.com>
2022-02-25 18:48:19 -0300

    imx8qm-var-som: linux-variscite: Update to latest
    Include the following:
    220925bf606d MLK-25674 mmc: sdhci-esdhc-imx: remove command queue feature support
    
    Signed-off-by: Alifer Moraes <alifer.m@variscite.com>

--- variscite/imx8qm-var-som/imx8qm-var-som.sh
+++ variscite/imx8qm-var-som/imx8qm-var-som.sh
@@ -12,7 +12,7 @@ readonly G_UBOOT_NAME_FOR_EMMC='imx-boot-sd.bin'
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="imx_4.14.98_2.0.0_ga_var01"
-readonly G_LINUX_KERNEL_REV="0626aeefd40a94c395c9c5e450a961f24e1e9096"
+readonly G_LINUX_KERNEL_REV="220925bf606d0f7259facbe130be875965fef9c6"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx8_var_defconfig'
 G_LINUX_DTB="freescale/fsl-imx8qm-var-som-dp.dtb
 	freescale/fsl-imx8qm-var-som-hdmi.dtb
