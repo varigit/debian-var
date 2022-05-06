301c90248165d377f0c25e53183a3319861ec26f
Alexander Herfurtner <alexander.h@variscite.com>
2022-05-06 21:42:42 +0200

    var-som-mx7: linux-variscite: update to latest revision
    Include the following commits:
    
    ca16ac29e58d imx7d-var-som: fec: add 20ms phy-reset-post-delay
    93717ca96352 imx8mm-var-dart: fec: add 20ms phy-reset-post-delay
    b979eeade770 MLK-25674 mmc: sdhci-esdhc-imx: remove command queue feature support
    cda7547f574e imx8mq-var-dart: Add PCIE PHY power regulator
    11459664a2bb MLK-25349-3 PCI: imx: clear vreg bypass when pcie vph voltage is 3v3
    c7e88ac830a9 MLK-25349-2 arm64: dts: imx8mq-evk: add one regulator used to power up pcie phy
    fe32f602d396 MLK-25349-1 dt-bindings: imx6q-pcie: add one regulator used to power up pcie phy
    
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>

--- variscite/var-som-mx7/var-som-mx7.sh
+++ variscite/var-som-mx7/var-som-mx7.sh
@@ -16,7 +16,7 @@ readonly G_SPL_NAME_FOR_NAND='SPL.nand'
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="imx_4.14.78_1.0.0_ga_var01"
-readonly G_LINUX_KERNEL_REV="a9b238a408145514b5c2de2c169e0992772f9423"
+readonly G_LINUX_KERNEL_REV="ca16ac29e58df9ecd25324144dd1dcf6d9bf5543"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_defconfig'
 G_LINUX_DTB="imx7d-var-som-emmc.dtb
 	imx7d-var-som-nand.dtb
