6d34a4912dab867c0228d76d99bfae85f9402276
Alexander Herfurtner <alexander.h@variscite.com>
2022-05-06 21:06:31 +0200

    linux-variscite: update to latest revision
    Include the following commits:
    
    48830b9e20f6 imx7d-var-som: fec: add 20ms phy-reset-post-delay
    a9f61190c68a net: fec: add post PHY reset delay DT property
    80ec273a9db0 imx6ull-var-dart.dtsi: fix pinctrl-names
    91a2d54e41dc ARM: dts: imx6ull: specify proper clocks for the PWM nodes
    930b72fd05ac imx6q-var-dart: Update the cpu operation points
    33e84a0bd034 ov5640:Fix YUV color space issue
    12e2d889ee45 ov5640: Fix camera module reset sequence for VAR-EXT-CB6UL
    a264544324c3 ARM: dts: imx6ull: update vdd_soc voltage for 900MHz operating point
    2e62d1a603a2 rtc: m41t80: fix race conditions
    0125bd08c220 rtc: m41t80: fix SQW dividers override when setting a date
    7b2f46eadb8a var-solocb-dt6cb-displays: reorganize crtc assignements
    
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>

--- make_var_som_mx7_debian.sh
+++ make_var_som_mx7_debian.sh
@@ -42,7 +42,7 @@ readonly G_VARISCITE_PATH="${DEF_BUILDENV}/variscite"
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/varigit/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="imx_4.9.11_1.0.0_ga-var01"
-readonly G_LINUX_KERNEL_REV="86c54d808ad9f1b39f8641d2da2e6da4bfff9f30"
+readonly G_LINUX_KERNEL_REV="48830b9e20f6fd98d77b83e71661343bdbfa42cb"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_defconfig'
 readonly G_LINUX_DTB='imx7d-var-som-emmc.dtb imx7d-var-som-emmc-m4.dtb imx7d-var-som-nand.dtb imx7d-var-som-nand-m4.dtb'
 
