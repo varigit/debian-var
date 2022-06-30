acb54069725e5f47000057f3fd781e66d3c46551
FrancescoFerraro <francesco.f@variscite.com>
2022-06-30 17:18:40 +0200

    imx8qm-var-som: u-boot-variscite: Update to latest
    Include the following commit:
    
    c67389b3e5 imx8qm_var_som: enable carrier board phy
    
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- variscite/imx8qm-var-som/imx8qm-var-som.sh
+++ variscite/imx8qm-var-som/imx8qm-var-som.sh
@@ -4,7 +4,7 @@ readonly ARCH_CPU="64BIT"
 readonly G_UBOOT_SRC_DIR="${DEF_SRC_DIR}/uboot"
 readonly G_UBOOT_GIT="https://github.com/varigit/uboot-imx.git"
 readonly G_UBOOT_BRANCH="imx_v2018.03_4.14.98_2.0.0_ga_var01"
-readonly G_UBOOT_REV="153dcd35edfd20e70c45435a7aa65bb1a2c45f2e"
+readonly G_UBOOT_REV="c67389b3e5a88f299809771a8287d0cf01a2738a"
 G_UBOOT_DEF_CONFIG_MMC='imx8qm_var_som_defconfig'
 readonly G_UBOOT_NAME_FOR_EMMC='imx-boot-sd.bin'
 
