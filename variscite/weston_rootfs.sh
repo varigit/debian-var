b36a08a985f54c2b3c17d3050799af43bd759513
FrancescoFerraro <francesco.f@variscite.com>
2023-05-10 12:25:28 +0200

    weston_rootfs.sh: align WIFI/BT scripts installation to Yocto latest
    With this patch, when porting a new machine, pay attention to:
    
    - get the variscite-wifi and variscite-bt scripts from latest meta-variscite-bsp
      repo. E.g. Kirkstone
    - remove  variscite-bt.conf and variscite-wifi.conf from machine folder
    - remove variscite-bt-common.sh and variscite-wifi-common.sh from machine folder
    
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- variscite/weston_rootfs.sh
+++ variscite/weston_rootfs.sh
@@ -115,11 +115,7 @@ function rootfs_install_var_bt {
 	install -m 0755 ${G_VARISCITE_PATH}/brcm_patchram_plus \
 		${ROOTFS_BASE}/usr/bin
 	install -d ${ROOTFS_BASE}/etc/bluetooth
-	install -m 0644 ${G_VARISCITE_PATH}/${MACHINE}/variscite-bt.conf \
-		${ROOTFS_BASE}/etc/bluetooth
-	install -m 0755 ${G_VARISCITE_PATH}/variscite-bt \
-		${ROOTFS_BASE}/etc/bluetooth
-	install -m 0755 ${G_VARISCITE_PATH}/variscite-bt-common.sh \
+	install -m 0755 ${G_VARISCITE_PATH}/${MACHINE}/variscite-bt \
 		${ROOTFS_BASE}/etc/bluetooth
 	install -m 0644 ${G_VARISCITE_PATH}/variscite-bt.service \
 		${ROOTFS_BASE}/lib/systemd/system
@@ -130,11 +126,7 @@ function rootfs_install_var_bt {
 function rootfs_install_var_wifi() {
 	# install variscite-wifi service
 	install -d ${ROOTFS_BASE}/etc/wifi
-	install -m 0644 ${G_VARISCITE_PATH}/${MACHINE}/variscite-wifi.conf \
-		${ROOTFS_BASE}/etc/wifi
-	install -m 0644 ${G_VARISCITE_PATH}/${MACHINE}/variscite-wifi-common.sh \
-		${ROOTFS_BASE}/etc/wifi
-	install -m 0755 ${G_VARISCITE_PATH}/variscite-wifi \
+	install -m 0755 ${G_VARISCITE_PATH}/${MACHINE}/variscite-wifi \
 		${ROOTFS_BASE}/etc/wifi
 	install -m 0644 ${G_VARISCITE_PATH}/variscite-wifi.service \
 		${ROOTFS_BASE}/lib/systemd/system
