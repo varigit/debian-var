a397cc9ec3267afd0788a4577fbc97fbd8e8d350
Harshesh Valera <harshesh.v@variscite.com>
2023-07-28 13:51:08 -0700

    imx8mp-var-dart: Enable imx8mp xcvr firmware
    Signed-off-by: Harshesh Valera <harshesh.v@variscite.com>

--- variscite/weston_rootfs.sh
+++ variscite/weston_rootfs.sh
@@ -34,6 +34,11 @@ copy_optional_package() {
 }
 
 
+# xcvr firmware package
+if [ ! -z "${XCVR_FIRMWARE_PACKAGE}" ]
+then
+	protected_install ${XCVR_FIRMWARE_PACKAGE}
+fi
 
 function rootfs_copy_packages() {
 	# copy common packages
